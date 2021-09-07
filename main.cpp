1ae254bbdc458697a84d06939f8eb9f05270e868
sidneyniuhtc <76147739+sidneyniuhtc@users.noreply.github.com>
2021-09-07 13:52:57 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <gtest/gtest.h>
 
+int Add(int x, int y) 
+{
+	return x + y;
+}
+
+TEST(AddTest, Negative) {
+	// This test is named "Negative", and belongs to the "FactorialTest"
+	// test case.
+	EXPECT_EQ(1, Add(0, 1));
+	EXPECT_EQ(5, Add(2, 3));
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	auto res = RUN_ALL_TESTS();
+	int x = 3;
 }
